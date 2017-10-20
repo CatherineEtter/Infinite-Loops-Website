e934b33c3c02eb46de63f994b1938c7a14ff57ec
Catherine Etter <cetter.catherine.web@gmail.com>
2017-10-19 19:48:45 -0500

    Changed JavaScript to display terms instead

--- infinite-loops-v1.js
+++ infinite-loops-v1.js
@@ -19,7 +19,6 @@ function activateContact() {
     $("#nav-contact").addClass("active");
 }
 function refreshClasses() {
-    var yearStart = "<p class=\"m-title-content\">";
     var classStart = "<p class=\"s-title-content\">"
     var descStart = "<p class=\"body-content\">";
     
@@ -29,23 +28,11 @@ function refreshClasses() {
         $("#class-display").empty();
         for(var i = 0; i < length; i++) {
             
-            if($("#year-1").is(":checked") && data[i].year == "Freshman") {
-                $("#class-display").append(yearStart + data[i].year + "</p>");
+            if($("#fall").is(":checked") && data[i].term == "Fall") {
                 $("#class-display").append(classStart + data[i].title + "</p>");
                 $("#class-display").append(descStart + data[i].description + "</p>");
             }
-            if($("#year-2").is(":checked") && data[i].year == "Sophomore") {
-                $("#class-display").append(yearStart + data[i].year + "</p>");
-                $("#class-display").append(classStart + data[i].title + "</p>");
-                $("#class-display").append(descStart + data[i].description + "</p>");
-            }
-            if($("#year-3").is(":checked") && data[i].year == "Junior") {
-                $("#class-display").append(yearStart + data[i].year + "</p>");
-                $("#class-display").append(classStart + data[i].title + "</p>");
-                $("#class-display").append(descStart + data[i].description + "</p>");
-            }
-            if($("#year-4").is(":checked") && data[i].year == "Senior") {
-                $("#class-display").append(yearStart + data[i].year + "</p>");
+            if($("#spring").is(":checked") && data[i].term == "Spring") {
                 $("#class-display").append(classStart + data[i].title + "</p>");
                 $("#class-display").append(descStart + data[i].description + "</p>");
             }
